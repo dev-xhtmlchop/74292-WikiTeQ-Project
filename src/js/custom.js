db2e33458843cd777c97049b5e51b97a9c2336ff
JituRaj007 <jitendragupta.xhtmlchop@gmail.com>
2025-05-14 19:13:42 +0530

    sub-menu updated - section wise scss seprated
    sub-menu updated - section wise scss seprated

--- src/js/custom.js
+++ src/js/custom.js
@@ -4,14 +4,33 @@
     var windowWidth = $(window).width();
     var containerWidth = $('.container').outerWidth();
     var value02 = (windowWidth - containerWidth) / 2 + 15;
-
     $('.cases-slider').css('padding-left', value02 + 'px'); 
   }
 
-  $(document).ready(function() {       
+  $(document).ready(function() {    
+    
+    $('.menu-item-has-children').each(function() {
+        $(this).append('<span class="dropdown"></span>');     
+    });
+    
     $('#hamburger').click(function() {
       $(this).toggleClass('open');
       $('.header-menus').slideToggle(300);
+    });
+
+    $('.menu-item-has-children > .dropdown').on('click', function(e) {
+      if ($(window).width() < 991) {
+        e.preventDefault();
+        const $clickedDropdown = $(this);
+        const $currentSubmenu = $clickedDropdown.siblings('.sub-menu');
+        const $parentItem = $clickedDropdown.parent();
+        $parentItem.siblings('.menu-item-has-children').each(function() {
+          $(this).find('> .sub-menu').slideUp();
+          $(this).find('> .dropdown').removeClass('active');
+        });
+        $currentSubmenu.slideToggle();
+        $clickedDropdown.toggleClass('active');
+      }
     });
 
     
@@ -25,18 +44,19 @@
       autoplaySpeed: 50000, 
       dots: true,
       responsive: [
-    {
-      breakpoint: 576,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        variableWidth: false,
-      }
-    }
-  ]
+        {
+          breakpoint: 576,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            variableWidth: false,
+          }
+        }
+      ]
     });
 
     updateDivPosition();
+    
     $(window).on('resize' , function () {
       updateDivPosition();
     });
@@ -45,4 +65,3 @@
 })(jQuery);
 
 
-//# sourceMappingURL=custom.js.map
